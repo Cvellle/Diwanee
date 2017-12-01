66f51e924b3a3e84866212cb3a03ac798221ca36
Cvellle <cuekisa@yahoo.com>
2017-12-01 20:46:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ var msie = ua.indexOf("MSIE ");
 
 if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number
 
-
+	window.scrollBy(0, 2000);
 	
   
 else { 
