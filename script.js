ae0ed3ba6b3ee70ec25bb31d9d22592524c6ec15
Cvellle <cuekisa@yahoo.com>
2017-12-02 19:48:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-const menButton = document.querySelector('.menuButton');
+
+  
+var menButton = document.querySelector('.menuButton');
 menButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
 	
 	this.nextElementSibling.style.transition="0.3s";
-/*
-    if (this.nextElementSibling.style.opacity=="0.985") {
-       this.nextElementSibling.style.opacity="0";
-	   this.nextElementSibling.style.display="none";
-      
-    } else {
-      this.nextElementSibling.style.opacity="0.985"
-	  this.nextElementSibling.style.display="visible";
-	}
-}
-*/
 
 
  if (this.nextElementSibling.style.display === "none") {
         this.nextElementSibling.style.display = "block";
-    } else {
+    } else
+		{
         this.nextElementSibling.style.display = "none";
     }
 	
-}
+}	  
+  
 
+var ua = window.navigator.userAgent;
+var msie = ua.indexOf("MSIE ");
+
+
+
+if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) 
+ 
+  {  
+ 
+bu1.addEventListener("click",function () {
+   window.scrollBy(0, 2850);
+});
+bu2.addEventListener("click",function () {
+   window.scrollBy(0, 1500);
+});   
+    
+bu3.addEventListener("click",function () {
+   window.scrollBy(0, 10000);
+}); 
+
+
+  }
+
+else 
+    
+
+
+jQuery(document).ready(function($) {
+
+    
+$(".b1").click(function(){
+
+		$("html, body").animate({ scrollTop: $('.description').offset().top }, 1700);		
+
+	});
+	
+
+$(".b2").click(function(){
+
+		$("html, body").animate({ scrollTop: $('.looking').offset().top }, 1700);		
+
+	});
+
+$(".b3").click(function(){
+
+		$("html, body").animate({ scrollTop: $('.contacts').offset().top }, 1700);		
+
+	});
+
+    
 
 
 
@@ -47,47 +90,7 @@ $("#button3").click(function(){
 
 	});
 
-
-
-
-
-
-
-$(".b1").click(function(){
-
-		$("html, body").animate({ scrollTop: $('.description').offset().top }, 1700);		
-
-	});
-	
-
-$(".b2").click(function(){
-
-		$("html, body").animate({ scrollTop: $('.looking').offset().top }, 1700);		
-
-	});
-
-$(".b3").click(function(){
-
-		$("html, body").animate({ scrollTop: $('.contacts').offset().top }, 1700);		
-
-	});
-
-
-	
-var ua = window.navigator.userAgent;
-var msie = ua.indexOf("MSIE ");
-
-if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer, return version number
-
-	window.scrollBy(0, 2000);
-	
-  
-else { 
-
-
-
-
-	
-}
-
-
+    
+    
+    
+});
